4a132a5dfed6193db0f18dbbf32f72818c211ec6
clintonixx <161770373+clintonixx@users.noreply.github.com>
2024-11-25 23:50:17 +0000

    generato-excuse

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandom(anyArray) {
+  let randomIndex = Math.floor(Math.random() * anyArray.length);
+  return anyArray[randomIndex];
+}
+
+function generateExcuse() {
+  return (
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when)
+  );
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
+ºº;
